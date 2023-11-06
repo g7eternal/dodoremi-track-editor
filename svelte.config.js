66ca72b04b9fcae9a591d497fe9f57ee3965ea7e
g7eternal <40625769+g7eternal@users.noreply.github.com>
2023-11-06 23:23:56 +0300

    add gh-pages support

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,12 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/dodoremi-track-editor" : "",
+    },
+    prerender: {
+      handleHttpError: "warn",
+    },
   },
 };
 
